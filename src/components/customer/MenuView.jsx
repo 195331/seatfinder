97d89f922ea6f3183afcc6a777c6d21056d5828f
Akshaj Patil <akshajnpatil@gmail.com>
2026-03-04 01:54:59 +0000

    File changes

--- src/components/customer/MenuView.jsx
+++ src/components/customer/MenuView.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Leaf, Wheat, Star } from 'lucide-react';
 import { Badge } from "@/components/ui/badge";
 import { cn } from "@/lib/utils";
